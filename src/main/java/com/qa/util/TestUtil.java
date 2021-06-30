57a3e2a724e866db6c5f85115a708a797b21d8b5
HP <HP@Algate>
2021-07-01 02:30:23 +0530

    Test

---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,8 @@
+package com.qa.util;
+
+public class TestUtil {
+	public void test() {
+		System.out.println("Test Utils");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,15 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void testlogin() {
+		
+	
+	 System.out.println("Testlogin"); 
+		
+
+	}
+}
